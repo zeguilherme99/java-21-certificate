caae79a774cebb28792492bd7a250028a6bd5d94
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-05-31 14:04:31 -0300

    first commit

---
+++ src/main/java/com/certificate/java/professional/ProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.certificate.java.professional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProfessionalApplication.class, args);
+  }
+
+}
