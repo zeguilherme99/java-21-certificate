100d544c1ca8fad93795bea43c46d6001a6114b5
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-01 17:59:19 -0300

    [variables] feat: variables

---
+++ src/main/java/com/certificate/java/professional/variables/Variables.java
@@ -0,0 +1,31 @@
+package com.certificate.java.professional.variables;
+
+public class Variables {
+
+  public static void main(String[] args) {
+
+    int a = 10, b = 20, c = a + b;
+
+    System.out.println("c: " + (a + b));
+
+    // use year instead y
+    // use month instead m
+
+    int age; //valid
+    int _age; //valid
+    int $age; //valid
+    int age3; //valid
+
+    /*
+    int a ge; //invalid
+    int 3age; //invalid
+     */
+
+    /*
+    int break;
+    int public;
+    int do;
+     */
+
+  }
+}
