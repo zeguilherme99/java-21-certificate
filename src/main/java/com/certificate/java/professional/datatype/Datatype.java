91d3d7bb38b307db1bf8c062a96ca6bb4f40af1b
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-01 18:17:15 -0300

    [datatype] feat: datatype

---
+++ src/main/java/com/certificate/java/professional/datatype/Datatype.java
@@ -0,0 +1,32 @@
+package com.certificate.java.professional.datatype;
+
+public class Datatype {
+
+  public static void main(String[] args) {
+
+    boolean a = true;
+    char b = 'B';
+    byte c = 12;
+    short d = -346;
+    int e = 435252;
+    long f = 536463L;
+    float g = 2462624.000F;
+    double h = 41412421.00;
+
+    System.out.println("a: " + a);
+    System.out.println("b: " + b);
+    System.out.println("c: " + c);
+    System.out.println("d: " + d);
+    System.out.println("e: " + e);
+    System.out.println("f: " + f);
+    System.out.println("g: " + g);
+    System.out.println("h: " + h);
+
+    //String class
+    String str1 = "This is a string";
+    System.out.println("str1 = " + str1);
+
+    //some tips
+    long money = 9_999_999_999L;
+  }
+}
