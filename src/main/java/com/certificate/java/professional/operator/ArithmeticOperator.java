0a9c9df4bdf0ef701fb7a3f7ef1ebca222b52137
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-03 15:40:29 -0300

    [arithmetic-operator] feat: arithmetic operators

---
+++ src/main/java/com/certificate/java/professional/operator/ArithmeticOperator.java
@@ -0,0 +1,24 @@
+package com.certificate.java.professional.operator;
+
+public class ArithmeticOperator {
+
+  public static void main(String[] args) {
+
+    double number1 = 8.4, number2 = 4.2, result = 0.0;
+
+    result = number1 + number2;
+    System.out.println("number1 + number2 = " + result);
+
+    result = number1 - number2;
+    System.out.println("number1 - number2 = " + result);
+
+    result = number1 * number2;
+    System.out.println("number1 * number2 = " + result);
+
+    result = number1 / number2;
+    System.out.println("number1 / number2 = " + result);
+
+    result = number1 % number2;
+    System.out.println("number1 % number2 = " + result);
+  }
+}
