e892d16f8d88baeb73a437cc6463c41d4df9b053
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-03 15:55:20 -0300

    [unary-operator] feat: unary operators

---
+++ src/main/java/com/certificate/java/professional/operator/UnaryOperator.java
@@ -0,0 +1,26 @@
+package com.certificate.java.professional.operator;
+
+public class UnaryOperator {
+
+  public static void main(String[] args) {
+
+    double number =7.6;
+    boolean isCheck = true;
+
+    System.out.println("+ number : " + +number);
+    System.out.println("- number : " + -number);
+    System.out.println("++number : " + ++number);
+    System.out.println("--number : " + --number);
+    System.out.println("!isCheck : " + !isCheck);
+
+    System.out.println("-------------");
+
+    double result = 6.7;
+
+    System.out.println("result = " + result);
+    System.out.println("result++ =" + result++);
+    System.out.println("result =" + result);
+    System.out.println("++result =" + ++result);
+    System.out.println("result =" + result);
+  }
+}
