bfb273896db931b7dbcdea784f6c1bfe34e59d84
zeguilherme99 <souza.jose.guilherme@outlook.com>
2025-06-03 16:00:47 -0300

    [equality-relational-operator] feat: equality relational operators

---
+++ src/main/java/com/certificate/java/professional/operator/EqualityRelationalOperator.java
@@ -0,0 +1,16 @@
+package com.certificate.java.professional.operator;
+
+public class EqualityRelationalOperator {
+
+  public static void main(String[] args) {
+
+    int a = 2, b =5;
+
+    System.out.println("a == b " + (a==b));
+    System.out.println("a != b " + (a!=b));
+    System.out.println("a > b " + (a>b));
+    System.out.println("a < b " + (a<b));
+    System.out.println("a <= b " + (a<=b));
+    System.out.println("a >= b " + (a>=b));
+  }
+}
